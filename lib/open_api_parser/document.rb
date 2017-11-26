16723da89c64b83ef8b2c6b21b2001dfaec3b3cf
Marica Odagaki <ento.entotto@gmail.com>
2017-11-25 22:10:08 -0800

    Rename for consistency
    `path` could mean the path portion of a URI.

--- lib/open_api_parser/document.rb
+++ lib/open_api_parser/document.rb
@@ -19,32 +19,32 @@ module OpenApiParser
 
     private
 
-    def deeply_expand_refs(fragment, cur_path)
-      fragment, cur_path = expand_refs(fragment, cur_path)
+    def deeply_expand_refs(fragment, current_pointer)
+      fragment, current_pointer = expand_refs(fragment, current_pointer)
 
       if fragment.is_a?(Hash)
         fragment.reduce({}) do |hash, (k, v)|
-          hash.merge(k => deeply_expand_refs(v, "#{cur_path}/#{k}"))
+          hash.merge(k => deeply_expand_refs(v, "#{current_pointer}/#{k}"))
         end
       elsif fragment.is_a?(Array)
-        fragment.map { |e| deeply_expand_refs(e, cur_path) }
+        fragment.map { |e| deeply_expand_refs(e, current_pointer) }
       else
         fragment
       end
     end
 
-    def expand_refs(fragment, cur_path)
+    def expand_refs(fragment, current_pointer)
       if fragment.is_a?(Hash) && fragment.key?("$ref")
         raw_uri = fragment["$ref"]
         ref = OpenApiParser::Reference.new(raw_uri)
-        fully_resolved = ref.resolve(@path, cur_path, @content, @file_cache)
+        fully_resolved = ref.resolve(@path, current_pointer, @content, @file_cache)
         unless fully_resolved
           expand_refs(ref.referrent_document, ref.referrent_pointer)
         else
           [ref.referrent_document, ref.referrent_pointer]
         end
       else
-        [fragment, cur_path]
+        [fragment, current_pointer]
       end
     end
   end
