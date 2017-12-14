1ea0e734bd772991e75c229987b3556929add579
Marica Odagaki <ento.entotto@gmail.com>
2017-12-13 23:24:58 -0800

    Prefer using the test cases from the spec verbatim

--- spec/open_api_parser/pointer_spec.rb
+++ spec/open_api_parser/pointer_spec.rb
@@ -17,45 +17,49 @@ RSpec.describe OpenApiParser::Pointer do
   describe "resolve" do
     it "works with RFC examples" do
       resolutions = {
-        "" => DOCUMENT,
-        "/foo" => ["bar", "baz"],
-        "/foo/0" => "bar",
-        "/" => 0,
-        "/a~1b" => 1,
-        "/c%d" => 2,
-        "/e^f" => 3,
-        "/g|h" => 4,
-        "/i\\j" => 5,
-        "/k\"l" => 6,
-        "/ " => 7,
-        "/m~0n" => 8,
+        "#" => DOCUMENT,
+        "#/foo" => ["bar", "baz"],
+        "#/foo/0" => "bar",
+        "#/" => 0,
+        "#/a~1b" => 1,
+        "#/c%d" => 2,
+        "#/e^f" => 3,
+        "#/g|h" => 4,
+        "#/i\\j" => 5,
+        "#/k\"l" => 6,
+        "#/ " => 7,
+        "#/m~0n" => 8,
       }
 
       resolutions.each do |pointer, expected|
+        # with the pound sign
         expect(OpenApiParser::Pointer.new(pointer).resolve(DOCUMENT)).to eq(expected)
-        expect(OpenApiParser::Pointer.new('#' + pointer).resolve(DOCUMENT)).to eq(expected)
+        # without the pound sign
+        expect(OpenApiParser::Pointer.new(pointer[1..-1]).resolve(DOCUMENT)).to eq(expected)
       end
     end
 
     it "works with escaped RFC examples" do
       resolutions = {
-        "" => DOCUMENT,
-        "/foo" => ["bar", "baz"],
-        "/foo/0" => "bar",
-        "/" => 0,
-        "/a~1b" => 1,
-        "/c%25d" => 2,
-        "/e%5Ef" => 3,
-        "/g%7Ch" => 4,
-        "/i%5Cj" => 5,
-        "/k%22l" => 6,
-        "/%20" => 7,
-        "/m~0n" => 8,
+        "#" => DOCUMENT,
+        "#/foo" => ["bar", "baz"],
+        "#/foo/0" => "bar",
+        "#/" => 0,
+        "#/a~1b" => 1,
+        "#/c%25d" => 2,
+        "#/e%5Ef" => 3,
+        "#/g%7Ch" => 4,
+        "#/i%5Cj" => 5,
+        "#/k%22l" => 6,
+        "#/%20" => 7,
+        "#/m~0n" => 8,
       }
 
       resolutions.each do |pointer, expected|
+        # with the pound sign
         expect(OpenApiParser::Pointer.new(pointer).resolve(DOCUMENT)).to eq(expected)
-        expect(OpenApiParser::Pointer.new('#' + pointer).resolve(DOCUMENT)).to eq(expected)
+        # without the pound sign
+        expect(OpenApiParser::Pointer.new(pointer[1..-1]).resolve(DOCUMENT)).to eq(expected)
       end
     end
   end
